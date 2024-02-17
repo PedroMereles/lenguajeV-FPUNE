fd89796f6bbb94bebefec01a29ae14d89c0964cc
Pedro Mereles <pedromereles83@gmail.com>
2024-02-17 00:47:57 -0300

    enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBXsRg54C5GAcw_cbkZmtuJVHp0KOl9NnM",
+    authDomain: "lenguajev-a1488.firebaseapp.com",
+    projectId: "lenguajev-a1488",
+    storageBucket: "lenguajev-a1488.appspot.com",
+    messagingSenderId: "852426083867",
+    appId: "1:852426083867:web:8f6ca5b3b45789983fb2be"
+  }
 };
 
 /*
